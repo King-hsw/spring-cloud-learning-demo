36b89ee64351237ba68116fb9094cba6f14771b6
hongsw <1125030435@qq.com>
2020-11-11 16:39:56 +0800

    【新增】Eureka微服务模块

---
+++ eureka/src/main/java/cn/teleware/springcloud/EurekaApplication.java
@@ -0,0 +1,18 @@
+package cn.teleware.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author : hongsw
+ * @date : 2020-11-10-15:42
+ * @descriptions : Eureka 注册中心-服务端
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
