31d5329bcbfee4762e11d8c28b5eb5007cb9fad2
hongsw <1125030435@qq.com>
2020-11-11 16:42:40 +0800

    【新增】config-server微服务模块

---
+++ config-server/src/main/java/cn/teleware/springcloud/ConfigApplication.java
@@ -0,0 +1,18 @@
+package cn.teleware.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author : hongsw
+ * @date : 2020-11-11-11:48
+ * @descriptions :
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
