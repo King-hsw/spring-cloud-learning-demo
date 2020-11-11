9ad7cfa6f7fc14317b548645cb7bb5dc264b18b8
hongsw <1125030435@qq.com>
2020-11-11 16:47:01 +0800

    【新增】zuul网关微服务模块

---
+++ zuul/src/main/java/cn/teleware/springcloud/ZuulApplication.java
@@ -0,0 +1,20 @@
+package cn.teleware.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author : hongsw
+ * @date : 2020-11-10-15:56
+ * @descriptions :
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
